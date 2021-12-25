29656592cfceced18927ade1a4c7e348984329b6
Luka Maret <maret.luka@gmail.com>
2021-12-25 12:06:55 +0100

    feat: start project

---
+++ src/main/java/com/lukamaret/main/GuiceModule.java
@@ -0,0 +1,12 @@
+package com.lukamaret.main;
+
+import com.google.inject.AbstractModule;
+
+public class GuiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+
+}
